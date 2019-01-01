8783f630eea3a7a63525d93216b14f60f949a764
SharingSmile <hapsoa@gmail.com>
2019-01-02 01:25:52 +0900

    update ppt

--- test-code/ppt-test.vue
+++ test-code/ppt-test.vue
@@ -1,65 +1,20 @@
 <template>
   <div class="ppt-root">
     <div class="ppt-menu">
-      <v-item-group>
-        <v-btn flat>
-          <v-icon>crop_din</v-icon>
-        </v-btn>
+      <v-toolbar>
         <v-menu offset-y>
           <v-btn flat slot="activator">
             <v-icon>crop_din</v-icon>
           </v-btn>
           <v-list>
-            <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="200" offset-x open-on-hover>
-              <!-- <v-list-tile @mouseover="test" @click="test">
+            <v-menu v-model="menu" :nudge-width="200" offset-x open-on-hover>
+              <v-list-tile slot="activator">
                 <v-list-tile-title>도형</v-list-tile-title>
-              </v-list-tile>-->
-              <v-btn slot="activator">도형</v-btn>
-
+              </v-list-tile>
+              <!-- <v-btn slot="activator">도형</v-btn> -->
               <v-card>
-                <v-list>
-                  <v-list-tile avatar>
-                    <v-list-tile-avatar>
-                      <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
-                    </v-list-tile-avatar>
-
-                    <v-list-tile-content>
-                      <v-list-tile-title>John Leider</v-list-tile-title>
-                      <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
-                    </v-list-tile-content>
-
-                    <v-list-tile-action>
-                      <v-btn :class="fav ? 'red--text' : ''" icon @click="fav = !fav">
-                        <v-icon>favorite</v-icon>
-                      </v-btn>
-                    </v-list-tile-action>
-                  </v-list-tile>
-                </v-list>
-
-                <v-divider></v-divider>
-
-                <v-list>
-                  <v-list-tile>
-                    <v-list-tile-action>
-                      <v-switch v-model="message" color="purple"></v-switch>
-                    </v-list-tile-action>
-                    <v-list-tile-title>Enable messages</v-list-tile-title>
-                  </v-list-tile>
-
-                  <v-list-tile>
-                    <v-list-tile-action>
-                      <v-switch v-model="hints" color="purple"></v-switch>
-                    </v-list-tile-action>
-                    <v-list-tile-title>Enable hints</v-list-tile-title>
-                  </v-list-tile>
-                </v-list>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-
-                  <v-btn flat @click="menu = false">Cancel</v-btn>
-                  <v-btn color="primary" flat @click="menu = false">Save</v-btn>
-                </v-card-actions>
+                <v-icon @click="drawMode">crop_din</v-icon>
+                <v-icon>panorama_fish_eye</v-icon>
               </v-card>
             </v-menu>
             <v-list-tile @mouseover="test">
@@ -84,10 +39,19 @@
             <v-icon>format_align_justify</v-icon>
           </v-btn>
         </v-btn-toggle>
-      </v-item-group>
+      </v-toolbar>
     </div>
-    <svg width="800" height="700">
-      <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow"></circle>
+    <svg width="800" height="700" :draw="isDrawing" @mousedown="startDrawing" @click="drawRect">
+      <!-- <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow"></circle> -->
+      <rect
+        v-for="(rect, index) in rects"
+        :key="index"
+        :x="rect.x"
+        :y="rect.y"
+        :width="rect.width"
+        :height="rect.height"
+        style="fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9"
+      ></rect>
     </svg>
   </div>
 </template>
@@ -97,21 +61,52 @@ export default {
   data() {
     return {
       toggle_exclusive: 2,
-      fav: true,
       menu: false,
-      message: false,
-      hints: true,
+      isDrawing: false,
+      // 좌표
+      startX: 0,
+      startY: 0,
+      // 네모
+      rects: [] as object[],
     };
   },
   methods: {
     test() {
       console.log('yop');
     },
+    drawMode() {
+      this.isDrawing = true;
+    },
+    startDrawing(event: any) {
+      this.startX = event.offsetX;
+      this.startY = event.offsetY;
+      console.log('startX, Y : ', this.startX, this.startY);
+
+    },
+    drawRect(event: any) {
+      if (this.isDrawing === true) {
+        // 네모를 그린다.
+        // 마우스 클릭시와 뗄시의 좌표를 알아야 한다.
+        this.rects.push({
+          x: this.startX,
+          y: this.startY,
+          width: Math.abs(event.offsetX - this.startX),
+          height: Math.abs(event.offsetY - this.startY),
+        });
+
+        this.isDrawing = false;
+      }
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
-// .ppt-root {
-// }
+.ppt-root {
+  svg {
+    &[draw] {
+      cursor: crosshair;
+    }
+  }
+}
 </style>
